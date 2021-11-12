214507c95f065de2024297d3b8e637eed031823b
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-11-12 17:28:15 +0700

    test: change return value of LPChainlinkAggregator from bnb to usd

--- tests/unit/LPChainlinkAggregator.test.ts
+++ tests/unit/LPChainlinkAggregator.test.ts
@@ -6,6 +6,7 @@ import { lpChainlinkAggregatorUnitTestFixture } from "../helpers";
 import { LPChainlinkAggregator } from "../../typechain/v8";
 import { MockContract } from "@eth-optimism/smock";
 import { assertBigNumberClose, assertBigNumberClosePercent } from "../helpers/assert";
+import { parseUnits } from "ethers/lib/utils";
 
 chai.use(solidity);
 const { expect } = chai;
@@ -28,56 +29,57 @@ describe("LPChainlinkAggregator", () => {
       const cases = [
         {
           // Mock USDT-BNB reserves as of 26/10/2021
-          token0Reserve: BigNumber.from("144297232309821388024114171"),
-          token1Reserve: BigNumber.from("297388328910698975602105"),
+          token0Reserve: BigNumber.from("149681165696323435998144252"),
+          token1Reserve: BigNumber.from("238536815945335816428077"),
           timestamp: "1635246615",
-          totalSupply: BigNumber.from("5516904403782558199235815"),
-          p0LatestAnswer: BigNumber.from("2060694178194711"),
-          p1LatestAnswer: ethers.utils.parseEther("1"),
-          totalBnb: BigNumber.from("297388328910698975602105").mul(2),
-          hasBnb: true,
+          totalSupply: BigNumber.from("4956871770040299418887006"),
+          p0LatestAnswer: BigNumber.from("100042528"),
+          p1LatestAnswer: BigNumber.from("62778480500"),
+          totalUSD: BigNumber.from("149681165696323435998144252").mul(2),
+          hasUSD: true,
         },
         {
           // Mock BTCB-BNB reserves as of 26/10/2021
-          token0Reserve: BigNumber.from("1683623474148470069646"),
-          token1Reserve: BigNumber.from("212300121413569965773199"),
+          token0Reserve: BigNumber.from("1145351984420813854759"),
+          token1Reserve: BigNumber.from("117672261716778619340198"),
           timestamp: "1635246615",
-          totalSupply: BigNumber.from("18072702183927274249304"),
-          p0LatestAnswer: BigNumber.from("126103706658610680000"),
-          p1LatestAnswer: ethers.utils.parseEther("1"),
-          totalBnb: BigNumber.from("212300121413569965773199").mul(2),
-          hasBnb: true,
+          totalSupply: BigNumber.from("11062389944077483611798"),
+          p0LatestAnswer: BigNumber.from("6477958711045"),
+          p1LatestAnswer: BigNumber.from("62778480500"),
+          totalUSD: constants.Zero,
+          hasUSD: false,
         },
         {
           // Mock BTCB-BUSD reserves as of 26/10/2021
-          token0Reserve: BigNumber.from("1040304577677254288105"),
-          token1Reserve: BigNumber.from("63460947420840552075704159"),
+          token0Reserve: BigNumber.from("890995832092113200734"),
+          token1Reserve: BigNumber.from("57630473172967486370481746"),
           timestamp: "1635246615",
-          totalSupply: BigNumber.from("240715535936845319007230"),
-          p0LatestAnswer: BigNumber.from("126103706658610680000"),
-          p1LatestAnswer: BigNumber.from("2061755576837038"),
-          totalBnb: constants.Zero,
-          hasBnb: false,
+          totalSupply: BigNumber.from("211747360381132745795516"),
+          p0LatestAnswer: BigNumber.from("6460448300000"),
+          p1LatestAnswer: BigNumber.from("100000000"),
+          totalUSD: BigNumber.from("57630473172967486370481746").mul(2),
+          hasUSD: true,
         },
       ];
 
       for (const _case of cases) {
         lattePair.smocked.getReserves.will.return.with([_case.token0Reserve, _case.token1Reserve, _case.timestamp]);
         lattePair.smocked.totalSupply.will.return.with(_case.totalSupply);
-        // Mock BNB price for px0
+        // Mock BUSD price for px0
         px0Aggregator.smocked.latestAnswer.will.return.with(_case.p0LatestAnswer);
-        // Mock BNB price for px1
+        // Mock BUSD price for px1
         px1Aggregator.smocked.latestAnswer.will.return.with(_case.p1LatestAnswer);
 
         assertBigNumberClosePercent(
-          _case.hasBnb
-            ? _case.totalBnb.mul(constants.WeiPerEther).div(_case.totalSupply).toString()
+          _case.hasUSD
+            ? _case.totalUSD.mul(constants.WeiPerEther).div(_case.totalSupply).toString()
             : _case.token0Reserve
-                .mul(_case.p0LatestAnswer)
-                .add(_case.token1Reserve.mul(_case.p1LatestAnswer))
+                .mul(_case.p0LatestAnswer.mul(parseUnits("1", 10)))
+                .add(_case.token1Reserve.mul(_case.p1LatestAnswer).mul(parseUnits("1", 10)))
                 .div(_case.totalSupply)
                 .toString(), // non-fair lp price, but should be ok for assertion tho
-          (await lpChainlinkAggregator.latestAnswer()).toString()
+          (await lpChainlinkAggregator.latestAnswer()).toString(),
+          "0.03"
         );
       }
     });
