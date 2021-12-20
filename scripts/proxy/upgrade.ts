eae7d09cde57e5239d168de7ababf9e594cfd0c5
mint-chocolate-frappuccino <mint.chocolate.frappuccino.kub@gmail.com>
2021-12-20 18:10:38 +0700

    chore: upgrade implementation contracts network files

--- scripts/proxy/upgrade.ts
+++ scripts/proxy/upgrade.ts
@@ -1,6 +1,14 @@
 import { ethers, network, upgrades } from "hardhat";
-import { LatteSwapYieldStrategy, LatteSwapYieldStrategy__factory } from "../../compiled-typechain/v8";
-import { Clerk, Clerk__factory } from "../../typechain/v8";
+import {
+  Clerk,
+  Clerk__factory,
+  FlatMarket,
+  FlatMarket__factory,
+  LatteSwapYieldStrategy,
+  LatteSwapYieldStrategy__factory,
+  PCSYieldStrategy,
+  PCSYieldStrategy__factory,
+} from "../../typechain/v8";
 import { getConfig, IDevelopConfig, withNetworkFile } from "../../utils";
 
 async function main() {
@@ -14,10 +22,15 @@ async function main() {
   Check all variables below before execute the deployment script
   */
   const config = getConfig();
-  const TARGET_CONTRACT = (config as IDevelopConfig).Clerk;
-  console.log(`>> Upgrading a Contract`);
-  const Contract = (await ethers.getContractFactory("Clerk", (await ethers.getSigners())[0])) as Clerk__factory;
-  const contract = (await upgrades.upgradeProxy(TARGET_CONTRACT, Contract)) as Clerk;
+  const TARGET_CONTRACT = (config as IDevelopConfig).FlatYieldStrategy["PCS"]["CAKE-BNB"];
+  console.log(`>> Upgrading a Contract ${TARGET_CONTRACT}`);
+  const Contract = (await ethers.getContractFactory(
+    "PCSYieldStrategy",
+    (
+      await ethers.getSigners()
+    )[0]
+  )) as PCSYieldStrategy__factory;
+  const contract = (await upgrades.upgradeProxy(TARGET_CONTRACT, Contract)) as PCSYieldStrategy;
   await contract.deployed();
   console.log(`✅ Done Upgrading a Contract`);
 }
